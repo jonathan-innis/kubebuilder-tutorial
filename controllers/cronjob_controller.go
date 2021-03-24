5af7506b4ba24c627cbbda8b587cb8df8131c5c3
Jonathan Innis <jonathan.innis.ji@gmail.com>
2021-03-23 22:34:29 -0700

    Update the controller manager with CronJob logic

--- controllers/cronjob_controller.go
+++ controllers/cronjob_controller.go
@@ -18,13 +18,24 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"sort"
+	"time"
 
 	batch "tutorial/api/v1"
+
+	"github.com/robfig/cron"
+
+	kbatch "k8s.io/api/batch/v1"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ref "k8s.io/client-go/tools/reference"
 )
 
 // CronJobReconciler reconciles a CronJob object
@@ -37,27 +48,293 @@ type CronJobReconciler struct {
 
 var (
 	scheduledTimeAnnotation = "batch.tutorial.kubebuilder.io/scheduled-at"
+	jobOwnerKey             = ".metadata.controller"
+	apiGVStr                = batch.GroupVersion.String()
 )
 
-// +kubebuilder:rbac:groups=batch.tutorial.kubebuilder.io,resources=cronjobs,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=batch.tutorial.kubebuilder.io,resources=cronjobs/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get
+// +kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get;update;patch
+
+var (
+	activeJobs     []*kbatch.Job
+	successfulJobs []*kbatch.Job
+	failedJobs     []*kbatch.Job
+)
 
 func (r *CronJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	log := r.Log.WithValues("cronjob", req.NamespacedName)
+	r.Log = r.Log.WithValues("cronjob", req.NamespacedName)
 
 	var cronJob batch.CronJob
 	if err := r.Get(ctx, req.NamespacedName, &cronJob); err != nil {
-		log.Error(err, "unable to fetch CronJob")
+		r.Log.Error(err, "unable to fetch CronJob")
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	return ctrl.Result{}, nil
+	var childJobs kbatch.JobList
+	if err := r.List(ctx, &childJobs, client.InNamespace(req.Namespace), client.MatchingFields{jobOwnerKey: req.Name}); err != nil {
+		r.Log.Error(err, "unable to list child Jobs")
+		return ctrl.Result{}, err
+	}
+
+	if err := r.UpdateStatus(ctx, &cronJob, &childJobs); err != nil {
+		r.Log.Error(err, "unable to update the status of the CronJob object")
+		return ctrl.Result{}, err
+	}
+	if err := r.Cleanup(ctx, &cronJob); err != nil {
+		r.Log.Error(err, "unable to cleanup the old jobs")
+		return ctrl.Result{}, err
+	}
+	if cronJob.Spec.Suspend != nil && *cronJob.Spec.Suspend {
+		r.Log.V(1).Info("cronjob suspended, skipping")
+		return ctrl.Result{}, nil
+	}
+	missedRun, nextRun, err := r.GetNextScheduled(&cronJob)
+	if err != nil {
+		r.Log.Error(err, "unable to figure out CronJob schedule")
+		return ctrl.Result{}, nil
+	}
+	scheduledResult := ctrl.Result{RequeueAfter: nextRun.Sub(r.Now())}
+	r.Log = r.Log.WithValues("now", r.Now(), "next run", nextRun)
+
+	if missedRun.IsZero() {
+		r.Log.V(1).Info("no upcoming scheduled times, sleeping until next")
+		return scheduledResult, nil
+	}
+
+	r.Log = r.Log.WithValues("current run", missedRun)
+	tooLate := false
+	if cronJob.Spec.StartingDeadlineSeconds != nil {
+		tooLate = missedRun.Add(time.Duration(*cronJob.Spec.StartingDeadlineSeconds) * time.Second).Before(r.Now())
+	}
+	if tooLate {
+		r.Log.V(1).Info("missed starting deadline for last run, sleeping till next")
+		return scheduledResult, nil
+	}
+
+	if cronJob.Spec.ConcurrencyPolicy == batch.ForbidConcurrent && len(activeJobs) > 0 {
+		r.Log.V(1).Info("concurrency policy blocks concurrent runs, skipping", "num active", len(activeJobs))
+		return scheduledResult, nil
+	}
+
+	if cronJob.Spec.ConcurrencyPolicy == batch.ReplaceConcurrent {
+		for _, activeJob := range activeJobs {
+			if err := r.Delete(ctx, activeJob, client.PropagationPolicy(metav1.DeletePropagationBackground)); client.IgnoreNotFound(err) != nil {
+				r.Log.Error(err, "unable to delete active job", "job", activeJob)
+				return ctrl.Result{}, err
+			}
+		}
+	}
+
+	job, err := r.ConstructCronJob(&cronJob, missedRun)
+	if err != nil {
+		r.Log.Error(err, "unable to construct job from template")
+		return scheduledResult, nil
+	}
+
+	if err := r.Create(ctx, job); err != nil {
+		r.Log.Error(err, "unable to create Job for CronJob", "job", job)
+		return ctrl.Result{}, err
+	}
+	r.Log.V(1).Info("created Job for CronJob run", "job", job)
+
+	return scheduledResult, nil
 }
 
-func (r *CronJobReconciler) SetupWithManager(mgr ctrl.Manager) error {
+func (r *CronJobReconciler) ConstructCronJob(cronJob *batch.CronJob, scheduledTime time.Time) (*kbatch.Job, error) {
+	name := fmt.Sprintf("%s-%d", cronJob.Name, scheduledTime.Unix)
+
+	job := &kbatch.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Labels:      make(map[string]string),
+			Annotations: make(map[string]string),
+			Name:        name,
+			Namespace:   cronJob.Namespace,
+		},
+		Spec: *cronJob.Spec.JobTemplate.Spec.DeepCopy(),
+	}
+	for k, v := range cronJob.Spec.JobTemplate.Annotations {
+		job.Annotations[k] = v
+	}
+	job.Annotations[scheduledTimeAnnotation] = scheduledTime.Format(time.RFC3339)
+	for k, v := range cronJob.Spec.JobTemplate.Labels {
+		job.Labels[k] = v
+	}
+	if err := ctrl.SetControllerReference(cronJob, job, r.Scheme); err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
+
+func (r *CronJobReconciler) GetNextScheduled(cronJob *batch.CronJob) (lastMissed time.Time, next time.Time, err error) {
+	sched, err := cron.ParseStandard(cronJob.Spec.Schedule)
+	if err != nil {
+		return time.Time{}, time.Time{}, fmt.Errorf("Unparseable schedule %q: %v", cronJob.Spec.Schedule, err)
+	}
+	var earliestTime time.Time
+	if cronJob.Status.LastScheduleTime != nil {
+		earliestTime = cronJob.Status.LastScheduleTime.Time
+	} else {
+		earliestTime = cronJob.ObjectMeta.CreationTimestamp.Time
+	}
+	if cronJob.Spec.StartingDeadlineSeconds != nil {
+		schedulingDeadline := time.Now().Add(-time.Second * time.Duration(*cronJob.Spec.StartingDeadlineSeconds))
+		if schedulingDeadline.After(earliestTime) {
+			earliestTime = schedulingDeadline
+		}
+	}
+	if earliestTime.After(time.Now()) {
+		return time.Time{}, sched.Next(time.Now()), nil
+	}
+
+	starts := 0
+	for t := sched.Next(earliestTime); !t.After(time.Now()); t = sched.Next(t) {
+		lastMissed = t
+		starts++
+		if starts > 100 {
+			return time.Time{}, time.Time{}, fmt.Errorf("Too many missed start times (> 100). Set or decrease .spec.startingDeadlineSeconds or check clock skew.")
+		}
+	}
+	return lastMissed, sched.Next(time.Now()), nil
+}
+
+func (r *CronJobReconciler) Cleanup(ctx context.Context, cronJob *batch.CronJob) error {
+	if cronJob.Spec.FailedJobsHistoryLimit != nil {
+		sort.Slice(failedJobs, func(i, j int) bool {
+			if failedJobs[i].Status.StartTime == nil {
+				return failedJobs[j].Status.StartTime != nil
+			}
+			return failedJobs[i].Status.StartTime.Before(failedJobs[j].Status.StartTime)
+		})
+		for i, job := range failedJobs {
+			if int32(i) >= int32(len(failedJobs))-*cronJob.Spec.FailedJobsHistoryLimit {
+				break
+			}
+			if err := r.Delete(ctx, job, client.PropagationPolicy(metav1.DeletePropagationBackground)); client.IgnoreNotFound(err) != nil {
+				r.Log.Error(err, "unable to delete old failed job", "job", job)
+			} else {
+				r.Log.V(0).Info("deleted old failed job", "job", job)
+			}
+		}
+	}
+
+	if cronJob.Spec.SuccessfulJobsHistoryLimit != nil {
+		sort.Slice(successfulJobs, func(i, j int) bool {
+			if successfulJobs[i].Status.StartTime == nil {
+				return successfulJobs[i].Status.StartTime.Before(successfulJobs[j].Status.StartTime)
+			}
+			return successfulJobs[i].Status.StartTime.Before(successfulJobs[j].Status.StartTime)
+		})
+		for i, job := range successfulJobs {
+			if int32(i) >= int32(len(successfulJobs))-*cronJob.Spec.SuccessfulJobsHistoryLimit {
+				break
+			}
+			if err := r.Delete(ctx, job, client.PropagationPolicy(metav1.DeletePropagationBackground)); err != nil {
+				r.Log.Error(err, "unable to delete old successful job", "job", job)
+			} else {
+				r.Log.V(0).Info("deleted old successful job", "job", job)
+			}
+		}
+	}
+	return nil
+}
+
+func (r *CronJobReconciler) UpdateStatus(ctx context.Context, cronJob *batch.CronJob, childJobs *kbatch.JobList) error {
+	// find the active list of jobs
+	var mostRecentTime *time.Time //find the last run so we can update the status
+
+	isJobFinished := func(job *kbatch.Job) (bool, kbatch.JobConditionType) {
+		for _, c := range job.Status.Conditions {
+			if (c.Type == kbatch.JobComplete || c.Type == kbatch.JobFailed) && c.Status == corev1.ConditionTrue {
+				return true, c.Type
+			}
+		}
+		return false, ""
+	}
+
+	getScheduledTimeForJob := func(job *kbatch.Job) (*time.Time, error) {
+		timeRaw := job.Annotations[scheduledTimeAnnotation]
+		if len(timeRaw) == 0 {
+			return nil, nil
+		}
+
+		timeParsed, err := time.Parse(time.RFC3339, timeRaw)
+		if err != nil {
+			return nil, err
+		}
+		return &timeParsed, nil
+	}
+
+	for i, job := range childJobs.Items {
+		_, finishedType := isJobFinished(&job)
+		switch finishedType {
+		case "":
+			activeJobs = append(activeJobs, &childJobs.Items[i])
+		case kbatch.JobFailed:
+			failedJobs = append(failedJobs, &childJobs.Items[i])
+		case kbatch.JobComplete:
+			successfulJobs = append(successfulJobs, &childJobs.Items[i])
+		}
+
+		scheduledTimeForJob, err := getScheduledTimeForJob(&job)
+		if err != nil {
+			r.Log.Error(err, "unable to parse schedule time for child job", "job", &job)
+			continue
+		}
+		if scheduledTimeForJob != nil {
+			if mostRecentTime == nil {
+				mostRecentTime = scheduledTimeForJob
+			} else if mostRecentTime.Before(*scheduledTimeForJob) {
+				mostRecentTime = scheduledTimeForJob
+			}
+		}
+	}
+
+	if mostRecentTime != nil {
+		cronJob.Status.LastScheduleTime = &metav1.Time{Time: *mostRecentTime}
+	} else {
+		cronJob.Status.LastScheduleTime = nil
+	}
+	cronJob.Status.Active = nil
+	for _, activeJob := range activeJobs {
+		jobRef, err := ref.GetReference(r.Scheme, activeJob)
+		if err != nil {
+			r.Log.Error(err, "unable to make reference to active job", "job", activeJob)
+			continue
+		}
+		cronJob.Status.Active = append(cronJob.Status.Active, *jobRef)
+	}
+
+	if err := r.Status().Update(ctx, cronJob); err != nil {
+		r.Log.Error(err, "unable to update CronJob status")
+		return err
+	}
+	return nil
+}
+
+func (r *CronJobReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager) error {
+	if r.Clock == nil {
+		r.Clock = realClock{}
+	}
+
+	if err := mgr.GetFieldIndexer().IndexField(ctx, &kbatch.Job{}, jobOwnerKey, client.IndexerFunc(func(rawObj client.Object) []string {
+		job := rawObj.(*kbatch.Job)
+		owner := metav1.GetControllerOf(job)
+		if owner == nil {
+			return nil
+		}
+
+		if owner.APIVersion != apiGVStr || owner.Kind != "CronJob" {
+			return nil
+		}
+
+		return []string{owner.Name}
+	})); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&batch.CronJob{}).
+		Owns(&kbatch.Job{}).
 		Complete(r)
 }
